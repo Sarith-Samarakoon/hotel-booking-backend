4b3f37c03abfd8d4afdcab1a906e7e5c3048499a
Sarith-Samarakoon <IT22207036@my.sliit.lk>
2024-11-28 14:03:47 +0530

    category ratings

--- models/category.js
+++ models/category.js
@@ -26,6 +26,12 @@ const categorySchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  ratings: {
+    type: Number, // Store the average rating as a number
+    min: 0, // Minimum value is 0
+    max: 5, // Maximum value is 5
+    default: 0, // Default rating is 0
+  },
 });
 
 const Category = mongoose.model("categories", categorySchema);
