fea7d1439b00e07b3231c5d6f6460c589bbcab58
Sarith-Samarakoon <IT22207036@my.sliit.lk>
2024-11-28 20:10:22 +0530

    Update staff.js

--- models/staff.js
+++ models/staff.js
@@ -9,10 +9,6 @@ const staffSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  department: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
     required: true,
