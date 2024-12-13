f7def6c9cb80a8491b2219777335870cda10c580
Sarith-Samarakoon <IT22207036@my.sliit.lk>
2024-12-14 05:05:47 +0530

    Page count updated

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -49,7 +49,7 @@ export function deleteCategory(req, res) {
 
 export function getCategories(req, res) {
   const page = parseInt(req.query.page) || 1; // Default to page 1
-  const limit = parseInt(req.query.limit) || 10; // Default to 10 categories per page
+  const limit = parseInt(req.query.limit) || 20; // Default to 10 categories per page
   const skip = (page - 1) * limit;
 
   Category.find()
--- controllers/galleryItemController.js
+++ controllers/galleryItemController.js
@@ -35,7 +35,7 @@ export function postGalleryItem(req, res) {
 
 export function getGalleryItems(req, res) {
   const page = parseInt(req.query.page) || 1; // Default to page 1
-  const limit = parseInt(req.query.limit) || 10; // Default to 10 items per page
+  const limit = parseInt(req.query.limit) || 50; // Default to 10 items per page
   const skip = (page - 1) * limit; // Calculate skip value
 
   GalleryItem.find()
