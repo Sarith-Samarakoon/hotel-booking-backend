ebf1fab51667ae51bba0a8a5d81a8b19fe2a8edb
Sarith-Samarakoon <IT22207036@my.sliit.lk>
2024-12-14 13:14:03 +0530

    Rooms items per page updated

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -74,7 +74,7 @@ export function findRoomById(req, res) {
 
 export function getRooms(req, res) {
   const page = parseInt(req.query.page) || 1; // Default to page 1
-  const limit = parseInt(req.query.limit) || 10; // Default to 5 items per page
+  const limit = parseInt(req.query.limit) || 100; // Default to 100 items per page
 
   const skip = (page - 1) * limit; // Calculate the number of documents to skip
 
